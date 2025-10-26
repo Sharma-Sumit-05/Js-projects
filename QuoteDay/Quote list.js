d87c7e273434906bf06e91e5f77f9bc14a28db45
Sharma-Sumit-05 <sumitsharma23803@gmail.co>
2025-10-26 20:51:52 +0530

    new quotes list file make

---
+++ QuoteDay/Quote list.js
@@ -0,0 +1,66 @@
+
+const quoteList =[
+  {
+    quotes: "Success is not final failure is not fatal It is the courage to continue that counts",
+    author: "Winston Churchill",
+  },
+  {
+quotes: "It is better to fail in originality than to succeed in imitation",
+author: "Herman melville",
+  },
+  {
+    quotes: "The road to success and the road to failure are almost exactly the same",
+    author: "Colin R.Davis",
+  },
+  {
+   quotes: "Success usually comes to those who are too busy to be looking for it.",
+   author: "Henty David Thoreau",
+  },
+   {
+   quotes: "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.",
+   author: "Dale Carnegei",
+  },
+   {
+   quotes: "I never dreamed about success. I worked for it",
+   author: "Estee Lauder",
+  },
+   {
+   quotes: "You can’t be that kid standing at the top of the waterslide, overthinking it. You have to go down the chute.",
+   author: "Tina Fey",
+  },
+   {
+   quotes: "Do not stop thinking of life as an adventure. You have no security unless you can live bravely, excitingly, imaginatively; unless you can choose a challenge instead of competence",
+   author: "Eleanor Roosevelt",
+  },
+   {
+   quotes: "Perfection is not attainable. But if we chase perfection we can catch excellence.",
+   author: "Vince Lombardi",
+  },
+   {
+   quotes: "Get a good idea and stay with it. Dog it, and work at it until it’s done right.",
+   author: "Walt Disney",
+  },
+   {
+   quotes: "Don’t let yesterday take up too much of today",
+   author: "Will Rogers",
+  },
+   {
+   quotes: "You learn more from failure than from success. Don’t let it stop you. Failure builds character.",
+   author: "Sumit Sharma",
+  },
+
+   {
+   quotes: "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you",
+   author: "Steve Jobs",
+  },
+
+   {
+   quotes: "Experience is a hard teacher because she gives the test first, the lesson afterward.",
+   author: "Vernon Sanders Laws",
+  },
+     {
+   quotes: "Hard work and efforts never betrays you",
+   author: "Sumit Sharma",
+  },
+
+];
